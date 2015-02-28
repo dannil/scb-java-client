a09302051ac1f6c41aa7acc847498931ca99dec5
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-02-28 17:57:52 +0100

    Query builder now uses Google Guava ArrayListMultimap

--- src/main/java/org/dannil/scbapi/utility/QueryBuilder.java
+++ src/main/java/org/dannil/scbapi/utility/QueryBuilder.java
@@ -2,33 +2,36 @@ package org.dannil.scbapi.utility;
 
 import java.util.List;
 
+import com.google.common.collect.ArrayListMultimap;
+
 public final class QueryBuilder<E, K> {
 
 	public QueryBuilder() {
 
 	}
 
-	public final String build(List<E> codes, List<List<K>> values) {
+	public final String build(ArrayListMultimap<E, K> map) {
 		StringBuilder builder = new StringBuilder();
 		builder.append("{");
 		builder.append("\"query\": [");
-		for (int i = 0; i < codes.size(); i++) {
+		int i = 0;
+		for (E code : map.keySet()) {
 			builder.append("{");
-			builder.append("\"code\": \"" + codes.get(i) + "\",");
+			builder.append("\"code\": \"" + code + "\",");
 			builder.append("\"selection\": {");
 			builder.append("\"filter\": \"item\",");
 			builder.append("\"values\": [");
-			List<K> data = values.get(i);
-			for (int j = 0; j < data.size(); j++) {
-				builder.append("\"" + data.get(j) + "\"");
-				if (j != data.size() - 1) {
+			List<K> values = map.get(code);
+			for (int j = 0; j < values.size(); j++) {
+				builder.append("\"" + values.get(j) + "\"");
+				if (j != values.size() - 1) {
 					builder.append(",");
 				}
 			}
 			builder.append("]");
 			builder.append("}");
 			builder.append("}");
-			if (i != codes.size() - 1) {
+			if (i != map.keySet().size() - 1) {
 				builder.append(",");
 			}
 		}
