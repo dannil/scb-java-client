52595161c65b4c1ce9bc1c5a0e1f40c5e6f4a32c
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-03-01 22:45:35 +0100

    Changed test

--- src/test/java/org/dannil/scbapi/test/Test.java
+++ src/test/java/org/dannil/scbapi/test/Test.java
@@ -12,29 +12,38 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		SCBAPI api = new SCBAPI(new Locale("sv"));
+		Locale locale = new Locale("sv");
+		SCBAPI api = new SCBAPI(locale);
 
 		PopulationCollection collection = api.population().getPopulationBetweenYears(2001, 2003);
 		for (Population p : collection.getPopulations()) {
-			System.out.println(p);
+			// System.out.println(p);
 		}
 
-		// WORKS
+		// // WORKS
 		List<String> regions = new ArrayList<String>();
 		regions.add("1265");
 		regions.add("1267");
 		PopulationCollection collection2 = api.population().getPopulationForRegions(regions);
 		for (Population p : collection2.getPopulations()) {
-			System.out.println(p);
+			// System.out.println(p);
 		}
-
-		// WORKS
+		//
+		List<Integer> years5 = new ArrayList<Integer>();
+		years5.add(2001);
+		years5.add(2002);
+		PopulationCollection collection5 = api.population().getPopulationForRegions(regions, years5);
+		for (Population p : collection5.getPopulations()) {
+			// System.out.println(p);
+		}
+		//
+		// // WORKS
 		List<Integer> years = new ArrayList<Integer>();
 		years.add(2001);
 		years.add(2002);
 		PopulationCollection collection3 = api.population().getPopulationForYears(years);
 		for (Population p : collection3.getPopulations()) {
-			System.out.println(p);
+			// System.out.println(p);
 		}
 
 		// WORKS
@@ -43,7 +52,11 @@ public class Test {
 			System.out.println(p);
 		}
 
-		api.test();
+		// WORKS
+		// Svedala for life!
+		PopulationCollection collection6 = api.population().getPopulationForRegion("1263");
+		for (Population p : collection6.getPopulations()) {
+			// System.out.println(p);
+		}
 	}
-
 }
