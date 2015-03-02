721f55f3001625abdd527c02d87c563d75922294
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-03-02 20:14:14 +0100

    Added methods to interface

--- src/main/java/org/dannil/scbapi/AreaOperations.java
+++ src/main/java/org/dannil/scbapi/AreaOperations.java
@@ -1,5 +1,13 @@
 package org.dannil.scbapi;
 
+import java.util.List;
+
+import org.dannil.scbapi.model.AreaCollection;
+
 public interface AreaOperations {
+
+	public AreaCollection getArea();
+
+	public AreaCollection getArea(List<String> regions, List<String> types, List<Integer> years);
 
 }
