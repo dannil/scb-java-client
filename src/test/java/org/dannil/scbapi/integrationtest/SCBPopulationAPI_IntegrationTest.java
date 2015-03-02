b150285bbb2e4d459f31d0097d6f48ff9c6b278d
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-03-02 11:21:11 +0100

    Added fetching region and years dynamically

--- src/test/java/org/dannil/scbapi/integrationtest/SCBPopulationAPI_IntegrationTest.java
+++ src/test/java/org/dannil/scbapi/integrationtest/SCBPopulationAPI_IntegrationTest.java
@@ -2,10 +2,11 @@ package org.dannil.scbapi.integrationtest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.dannil.scbapi.SCBAPI;
 import org.dannil.scbapi.SCBPopulationAPI;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -13,24 +14,25 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class SCBPopulationAPI_IntegrationTest {
 
+	private SCBAPI coreApi;
 	private SCBPopulationAPI populationApi;
 
-	private List<String> regions;
+	private List<String> regionCodes;
+	private List<String> regionTexts;
 	private List<Integer> years;
 
-	@Before
-	public final void init() {
+	public SCBPopulationAPI_IntegrationTest() {
+		this.coreApi = new SCBAPI();
 		this.populationApi = new SCBPopulationAPI();
 
-		this.regions = new ArrayList<String>();
-		this.regions.add("1263");
-		this.regions.add("1267");
-		this.regions.add("00");
+		this.regionCodes = new ArrayList<String>();
+		Map<String, String> map = this.coreApi.getRegionMappings();
+		for (String code : map.keySet()) {
+			this.regionCodes.add(code);
+			this.regionTexts.add(map.get(code));
+		}
 
-		this.years = new ArrayList<Integer>();
-		this.years.add(1987);
-		this.years.add(1993);
-		this.years.add(2001);
+		this.years = this.populationApi.getAvailableYears();
 	}
 
 	@Test
@@ -40,27 +42,27 @@ public class SCBPopulationAPI_IntegrationTest {
 
 	@Test
 	public final void getPopulationForRegionNotNull() {
-		Assert.assertNotEquals(0, this.populationApi.getPopulationForRegion(this.regions.get(0)).getPopulations().size());
+		Assert.assertNotEquals(0, this.populationApi.getPopulationForRegion(this.regionCodes.get(0)).getPopulations().size());
 	}
 
 	@Test
 	public final void getPopulationForRegionAndYearsNotNull() {
-		Assert.assertNotEquals(0, this.populationApi.getPopulationForRegion(this.regions.get(0), this.years).getPopulations().size());
+		Assert.assertNotEquals(0, this.populationApi.getPopulationForRegion(this.regionCodes.get(0), this.years).getPopulations().size());
 	}
 
 	@Test
 	public final void getPopulationForRegionAndYearNotNull() {
-		Assert.assertNotEquals(0, this.populationApi.getPopulationForRegion(this.regions.get(0), this.years.get(0)).getPopulations().size());
+		Assert.assertNotEquals(0, this.populationApi.getPopulationForRegion(this.regionCodes.get(0), this.years.get(0)).getPopulations().size());
 	}
 
 	@Test
 	public final void getPopulationForRegions() {
-		Assert.assertNotEquals(0, this.populationApi.getPopulationForRegions(this.regions).getPopulations().size());
+		Assert.assertNotEquals(0, this.populationApi.getPopulationForRegions(this.regionCodes).getPopulations().size());
 	}
 
 	@Test
 	public final void getPopulationForRegionsAndYearsNotNull() {
-		Assert.assertNotEquals(0, this.populationApi.getPopulationForRegions(this.regions, this.years).getPopulations().size());
+		Assert.assertNotEquals(0, this.populationApi.getPopulationForRegions(this.regionCodes, this.years).getPopulations().size());
 	}
 
 	@Test
