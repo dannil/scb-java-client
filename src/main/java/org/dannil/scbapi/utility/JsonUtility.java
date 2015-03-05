9ddefbfb36c0e05091f0603afac83b6ed6c8a766
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-03-05 10:48:21 +0100

    Added logic for json node parsing

--- src/main/java/org/dannil/scbapi/utility/JsonUtility.java
+++ src/main/java/org/dannil/scbapi/utility/JsonUtility.java
@@ -7,11 +7,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonUtility {
 
+	public static final JsonNode getNode(String json) {
+		return getNode(json, null);
+	}
+
 	public static final JsonNode getNode(String json, String field) {
 		try {
 			ObjectMapper mapper = new ObjectMapper();
 			JsonNode node = mapper.readTree(json);
-			return node.get("data");
+			if (field != null) {
+				return node.get(field);
+			}
+			return node;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
