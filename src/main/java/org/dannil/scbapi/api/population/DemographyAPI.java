0842c0c28c6b00643868dee824ae16f7906494a7
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-03-06 11:05:51 +0100

    Added logic to models

--- src/main/java/org/dannil/scbapi/api/population/DemographyAPI.java
+++ src/main/java/org/dannil/scbapi/api/population/DemographyAPI.java
@@ -1,5 +1,137 @@
 package org.dannil.scbapi.api.population;
 
-public final class DemographyAPI {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import org.dannil.scbapi.api.AbstractAPI;
+import org.dannil.scbapi.model.AverageAgeFirstChild;
+import org.dannil.scbapi.utility.QueryBuilder;
+import org.dannil.scbapi.utility.RequestPoster;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ArrayListMultimap;
+
+public final class DemographyAPI extends AbstractAPI implements DemographyOperations {
+
+	private String url;
+
+	public DemographyAPI() {
+		this.locale = Locale.getDefault();
+
+		buildUrl();
+	}
+
+	public DemographyAPI(Locale locale) {
+		this();
+		this.locale = locale;
+
+		buildUrl();
+	}
+
+	@Override
+	public final void setLocale(Locale locale) {
+		this.locale = locale;
+
+		buildUrl();
+	}
+
+	public final void buildUrl() {
+		this.url = "http://api.scb.se/OV0104/v1/doris/" + this.locale.getLanguage() + "/ssd/BE/BE0701/MedelAlderNY";
+	}
+
+	public final List<String> getAvailableRegions() {
+		String response = RequestPoster.doGet(this.url);
+
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			JsonNode node = mapper.readTree(response);
+			List<JsonNode> nodes = node.findValues("values");
+			node = nodes.get(0);
+
+			List<String> years = new ArrayList<String>(node.size());
+			for (int i = 0; i < node.size(); i++) {
+				years.add(node.get(i).asText());
+			}
+			return years;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public final List<Integer> getAvailableGenders() {
+		String response = RequestPoster.doGet(this.url);
+
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			JsonNode node = mapper.readTree(response);
+			List<JsonNode> nodes = node.findValues("values");
+			node = nodes.get(1);
+
+			List<Integer> genders = new ArrayList<Integer>(node.size());
+			for (int i = 0; i < node.size(); i++) {
+				genders.add(node.get(i).asInt());
+			}
+			return genders;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public final List<Integer> getAvailableYears() {
+		String response = RequestPoster.doGet(this.url);
+
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			JsonNode node = mapper.readTree(response);
+			List<JsonNode> nodes = node.findValues("values");
+			node = nodes.get(nodes.size() - 1);
+
+			List<Integer> years = new ArrayList<Integer>(node.size());
+			for (int i = 0; i < node.size(); i++) {
+				years.add(node.get(i).asInt());
+			}
+			return years;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public List<AverageAgeFirstChild> getAverageAgeFirstChild() {
+		return this.getAverageAgeFirstChild(null, null, null);
+	}
+
+	@Override
+	public List<AverageAgeFirstChild> getAverageAgeFirstChild(List<String> regions, List<Integer> genders, List<Integer> years) {
+		QueryBuilder<String, String> queryBuilder = new QueryBuilder<String, String>();
+
+		ArrayListMultimap<String, String> map = ArrayListMultimap.create();
+		map.put("ContentsCode", "BE0701AB");
+		if (regions != null) {
+			for (String region : regions) {
+				map.put("Region", region);
+			}
+		}
+		if (genders != null) {
+			for (Integer gender : genders) {
+				map.put("Kon", gender.toString());
+			}
+		}
+		if (years != null) {
+			for (Integer year : years) {
+				map.put("Tid", year.toString());
+			}
+		}
+
+		String query = queryBuilder.build(map);
+		String response = RequestPoster.doPost(this.url, query);
+		return null;
+	}
 
 }
--- src/main/java/org/dannil/scbapi/model/AverageAgeFirstChild.java
+++ src/main/java/org/dannil/scbapi/model/AverageAgeFirstChild.java
@@ -3,17 +3,17 @@ package org.dannil.scbapi.model;
 public final class AverageAgeFirstChild {
 
 	private String region;
-	private Integer year;
 	private Integer gender;
+	private Integer year;
 
 	public AverageAgeFirstChild() {
 
 	}
 
-	public AverageAgeFirstChild(String region, Integer year, Integer gender) {
+	public AverageAgeFirstChild(String region, Integer gender, Integer year) {
 		this.region = region;
-		this.year = year;
 		this.gender = gender;
+		this.year = year;
 	}
 
 	public final String getRegion() {
@@ -24,20 +24,20 @@ public final class AverageAgeFirstChild {
 		this.region = region;
 	}
 
-	public final Integer getYear() {
-		return this.year;
-	}
-
-	public final void setYear(Integer year) {
-		this.year = year;
-	}
-
 	public final Integer getGender() {
 		return this.gender;
 	}
 
 	public final void setGender(Integer gender) {
 		this.gender = gender;
+	}
+
+	public final Integer getYear() {
+		return this.year;
+	}
+
+	public final void setYear(Integer year) {
+		this.year = year;
 	}
 
 }
