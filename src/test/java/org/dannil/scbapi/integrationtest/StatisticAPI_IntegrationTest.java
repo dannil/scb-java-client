5fc37c5c6d28a5eb0efb304d5c9568e3236effe2
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-03-15 09:55:56 +0100

    Added integration tests

--- src/test/java/org/dannil/scbapi/integrationtest/StatisticAPI_IntegrationTest.java
+++ src/test/java/org/dannil/scbapi/integrationtest/StatisticAPI_IntegrationTest.java
@@ -12,24 +12,38 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class SCBPopulationAPI_IntegrationTest {
+public class StatisticAPI_IntegrationTest {
 
-	private SCBAPI coreApi;
+	private SCBAPI api;
 	private StatisticAPI statisticsAPI;
 
-	private List<String> regionCodes;
-	private List<String> regionTexts;
+	private List<String> regions;
+	private List<String> relationshipStatuses;
+	private List<String> ages;
+	private List<Integer> genders;
 	private List<Integer> years;
 
 	@Before
 	public void initialize() {
-		this.coreApi = new SCBAPI();
-		this.statisticsAPI = this.coreApi.population().statistic();
+		this.api = new SCBAPI();
+		this.statisticsAPI = this.api.population().statistic();
 
-		this.regionCodes = new ArrayList<String>();
-		this.regionTexts = new ArrayList<String>();
+		this.regions = new ArrayList<String>();
+		this.regions.add("1263");
 
-		this.regionCodes.add("1263");
+		this.relationshipStatuses = new ArrayList<String>();
+		this.relationshipStatuses.add("OG");
+		this.relationshipStatuses.add("G");
+		this.relationshipStatuses.add("SK");
+		this.relationshipStatuses.add("ÄNKL");
+
+		this.ages = new ArrayList<String>();
+		this.ages.add("25");
+		this.ages.add("30");
+
+		this.genders = new ArrayList<Integer>();
+		this.genders.add(1);
+		this.genders.add(2);
 
 		this.years = new ArrayList<Integer>();
 		this.years.add(2002);
@@ -41,23 +55,48 @@ public class SCBPopulationAPI_IntegrationTest {
 	}
 
 	@Test
-	public final void getPopulationForRegionsNotNull() {
-		Assert.assertNotEquals(0, this.statisticsAPI.getPopulation(this.regionCodes, null, null, null, null).size());
+	public final void getPopulationForRegions() {
+		Assert.assertNotEquals(0, this.statisticsAPI.getPopulation(this.regions, null, null, null, null).size());
 	}
 
 	@Test
-	public final void getPopulationForRegionAndYearsNotNull() {
-		Assert.assertNotEquals(0, this.statisticsAPI.getPopulation(this.regionCodes, null, null, null, this.years).size());
+	public final void getPopulationForRegionsAndRelationshipStatuses() {
+		Assert.assertNotEquals(0, this.statisticsAPI.getPopulation(this.regions, this.relationshipStatuses, null, null, null).size());
 	}
 
 	@Test
-	public final void getPopulationBetweenYearsNotNull() {
+	public final void getPopulationForRegionsAndRelationshipStatusesAndAges() {
+		Assert.assertNotEquals(0, this.statisticsAPI.getPopulation(this.regions, this.relationshipStatuses, this.ages, null, null).size());
+	}
+
+	@Test
+	public final void getPopulationgetPopulationForRegionsAndRelationshipStatusesAndAgesAndGenders() {
+		Assert.assertNotEquals(0, this.statisticsAPI.getPopulation(this.regions, this.relationshipStatuses, this.ages, this.genders, null).size());
+	}
+
+	@Test
+	public final void getPopulationgetPopulationForRegionsAndRelationshipStatusesAndAgesAndGendersAndYears() {
+		Assert.assertNotEquals(0, this.statisticsAPI.getPopulation(this.regions, this.relationshipStatuses, this.ages, this.genders, this.years).size());
+	}
+
+	@Test
+	public final void getPopulationForRelationshipStatuses() {
+		Assert.assertNotEquals(0, this.statisticsAPI.getPopulation(null, this.relationshipStatuses, null, null, null).size());
+	}
+
+	@Test
+	public final void getPopulationForAges() {
+		Assert.assertNotEquals(0, this.statisticsAPI.getPopulation(null, null, this.ages, null, null).size());
+	}
+
+	@Test
+	public final void getPopulationForGenders() {
+		Assert.assertNotEquals(0, this.statisticsAPI.getPopulation(null, null, null, this.genders, null).size());
+	}
+
+	@Test
+	public final void getPopulationForYears() {
 		Assert.assertNotEquals(0, this.statisticsAPI.getPopulation(null, null, null, null, this.years).size());
-	}
-
-	@Test
-	public final void getPopulation() {
-
 	}
 
 }
