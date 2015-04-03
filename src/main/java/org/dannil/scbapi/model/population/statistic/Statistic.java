eaf5312027ada2cd9193f7a3248e4e48d59aec58
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-04-03 22:55:57 +0200

    Added null check

--- src/main/java/org/dannil/scbapi/model/population/statistic/Statistic.java
+++ src/main/java/org/dannil/scbapi/model/population/statistic/Statistic.java
@@ -55,6 +55,56 @@ public final class Statistic extends AbstractStatisticModel {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = super.hashCode();
+		result = prime * result + ((this.age == null) ? 0 : this.age.hashCode());
+		result = prime * result + ((this.amount == null) ? 0 : this.amount.hashCode());
+		result = prime * result + ((this.gender == null) ? 0 : this.gender.hashCode());
+		result = prime * result + ((this.relationshipStatus == null) ? 0 : this.relationshipStatus.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Statistic)) {
+			return false;
+		}
+		Statistic other = (Statistic) obj;
+		if (!super.equals(obj)) {
+			return false;
+		}
+		if (this.age == null) {
+			if (other.age != null) {
+				return false;
+			}
+		} else if (!this.age.equals(other.age)) {
+			return false;
+		}
+		if (this.amount == null) {
+			if (other.amount != null) {
+				return false;
+			}
+		} else if (!this.amount.equals(other.amount)) {
+			return false;
+		}
+		if (this.gender != other.gender) {
+			return false;
+		}
+		if (this.relationshipStatus != other.relationshipStatus) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 
@@ -62,9 +112,9 @@ public final class Statistic extends AbstractStatisticModel {
 		builder.append("[");
 		builder.append("region=" + super.region + ", ");
 		builder.append("year=" + super.year + ", ");
-		builder.append("relationshipStatus=" + this.relationshipStatus.toString() + ", ");
+		builder.append("relationshipStatus=" + (this.relationshipStatus != null ? this.relationshipStatus.toString() : null) + ", ");
 		builder.append("age=" + this.age + ", ");
-		builder.append("gender=" + this.gender.toString() + ", ");
+		builder.append("gender=" + (this.gender != null ? this.gender.toString() : null) + ", ");
 		builder.append("amount=" + this.amount);
 		builder.append("]");
 
