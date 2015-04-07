6d3c420526858b35bf73e1d2f91cd0f743e1fc99
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-04-07 19:53:21 +0200

    Updated model

--- src/main/java/org/dannil/scbapi/model/environment/landandwaterarea/Area.java
+++ src/main/java/org/dannil/scbapi/model/environment/landandwaterarea/Area.java
@@ -1,5 +1,8 @@
 package org.dannil.scbapi.model.environment.landandwaterarea;
 
+import java.util.Arrays;
+import java.util.List;
+
 public final class Area extends AbstractLandAndWaterAreaModel {
 
 	private String type;
@@ -34,6 +37,35 @@ public final class Area extends AbstractLandAndWaterAreaModel {
 	@Override
 	public String toString() {
 		return "Area [region=" + super.region + ", year=" + super.year + ", type=" + this.type + ", squareKm=" + this.squareKm + "]";
+	}
+
+	public static List<String> getCodes() {
+		return Arrays.asList("Region", "ArealTyp", "ContentsCode", "Tid");
+	}
+
+	public enum Type {
+		LANDAREA("01"), INLANDWATEREXCLUDINGTHEFOURLARGELAKES("02"), THEFOURLARGELAKES("03"), SEAWATER("04"), NULL(null);
+
+		private final String value;
+
+		private Type(String value) {
+			this.value = value;
+		}
+
+		public static Type of(Integer value) {
+			for (Type type : values()) {
+				if (type.value != null && type.value.equals(value)) {
+					return type;
+				}
+				return NULL;
+			}
+			throw new IllegalArgumentException(value.toString());
+		}
+
+		@Override
+		public String toString() {
+			return (this.value != null ? this.value.toString() : null);
+		}
 	}
 
 }
