b0055e078e7946ef2bbc79f070b83d4957ee74e0
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-04-08 20:50:19 +0200

    Fixed null conversion

--- src/main/java/org/dannil/scbapi/utility/ListUtility.java
+++ src/main/java/org/dannil/scbapi/utility/ListUtility.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class ListUtility<E> {
 
 	public static final <E> List<E> toList(E value) {
+		if (value == null) {
+			return null;
+		}
 		ArrayList<E> list = new ArrayList<E>(1);
 		list.add(value);
 		return list;
