a627dbb4f501297c364db856fd676b90b9cfb511
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-01-16 19:35:45 +0100

    Added unit test for SCBAPI

---
+++ src/test/java/com/github/dannil/scbapi/api/SCBAPI_UnitTest.java
@@ -0,0 +1,22 @@
+package com.github.dannil.scbapi.api;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Locale;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class SCBAPI_UnitTest {
+
+	@Test
+	public void createWithLocaleConstructor() {
+		Locale locale = new Locale("sv", "SE");
+		SCBAPI api = new SCBAPI();
+
+		assertEquals(locale, api.getLocale());
+	}
+
+}
