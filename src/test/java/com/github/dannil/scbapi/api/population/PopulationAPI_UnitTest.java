399948a181d615a814b98d3d7413f6de0dab4743
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-01-16 23:12:20 +0100

    Added test

---
+++ src/test/java/com/github/dannil/scbapi/api/population/PopulationAPI_UnitTest.java
@@ -0,0 +1,24 @@
+package com.github.dannil.scbapi.api.population;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Locale;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import com.github.dannil.scbapi.api.environment.landandwaterarea.LandAndWaterAreaAPI;
+
+@RunWith(JUnit4.class)
+public class PopulationAPI_UnitTest {
+
+	@Test
+	public void createWithLocaleConstructor() {
+		Locale locale = new Locale("sv", "SE");
+		PopulationAPI api = new PopulationAPI(locale);
+
+		assertEquals(locale, api.getLocale());
+	}
+
+}
