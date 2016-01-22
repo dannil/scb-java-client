ee3817191ae8cde738f8c43ec504ffa125997ae8
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-01-22 22:26:26 +0100

    Added tests

--- src/test/java/com/github/dannil/scbapi/api/AbstractAPI_IntegrationTest.java
+++ src/test/java/com/github/dannil/scbapi/api/AbstractAPI_IntegrationTest.java
@@ -2,12 +2,19 @@ package com.github.dannil.scbapi.api;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import com.github.dannil.scbapi.utility.ListUtility;
+import com.github.dannil.scbapi.utility.QueryBuilder;
 
 @RunWith(JUnit4.class)
 public class AbstractAPI_IntegrationTest {
@@ -18,6 +25,57 @@ public class AbstractAPI_IntegrationTest {
 			super();
 		}
 
+		protected DummyAPI(Locale locale) {
+			super(locale);
+		}
+
+	}
+
+	@Test
+	public void toFallbackUrlGet() {
+		DummyAPI api = new DummyAPI(new Locale("en", "US"));
+
+		String url = "HE/HE0103/HE0103B/BefolkningAlder";
+
+		// This request is performed by a dummy API which is set to English (as
+		// specified in the setup method.
+		// This means that if we receive a response with Swedish text, we've
+		// used the fallback url.
+		String response = api.get(url);
+
+		assertTrue(response.contains("ålder"));
+		assertTrue(response.contains("kön"));
+		assertTrue(response.contains("boendeform"));
+		assertTrue(response.contains("tabellinnehåll"));
+		assertTrue(response.contains("år"));
+	}
+
+	@Test
+	public void toFallbackUrlPost() {
+		DummyAPI api = new DummyAPI(new Locale("en", "US"));
+
+		String url = "HE/HE0103/HE0103B/BefolkningAlder";
+
+		// Build the payload
+		QueryBuilder builder = QueryBuilder.getInstance();
+
+		Map<String, List<?>> map = new HashMap<String, List<?>>();
+		map.put("ContentsCode", ListUtility.toList("HE0103D2"));
+		map.put("Alder", ListUtility.toList("tot"));
+		map.put("Kon", ListUtility.toList("4"));
+		map.put("Boendeform", ListUtility.toList("SMAG"));
+		map.put("Tid", ListUtility.toList("2012"));
+
+		// This request is performed by a dummy API which is set to English (as
+		// specified in the setup method.
+		// This means that if we receive a response with Swedish text, we've
+		// used the fallback url.
+		String response = api.post(url, builder.build(map));
+
+		assertTrue(response.contains("ålder"));
+		assertTrue(response.contains("kön"));
+		assertTrue(response.contains("boendeform"));
+		assertTrue(response.contains("år"));
 	}
 
 	@Test
