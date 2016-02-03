fc700788a7d23124be552f0cd4014969f1fd3c4b
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-02-03 13:18:54 +0100

    Now calculates time from the API

--- src/test/java/com/github/dannil/scbapi/test/utility/Config.java
+++ src/test/java/com/github/dannil/scbapi/test/utility/Config.java
@@ -1,8 +1,34 @@
 package com.github.dannil.scbapi.test.utility;
 
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.dannil.scbapi.utility.JsonUtility;
+import com.github.dannil.scbapi.utility.RequestPoster;
+
 public final class Config {
 
+	private static int timerMs;
+
+	static {
+		try {
+			String response = RequestPoster.doGet("http://api.scb.se/OV0104/v1/doris/en/ssd/?config");
+			JsonNode node = JsonUtility.getNode(response);
+
+			int maxCalls = node.get("maxCalls").asInt();
+			int timeWindow = node.get("timeWindow").asInt();
+
+			// 10000 10
+			timerMs = (int) (((timeWindow * 1000.0) / timeWindow) * (maxCalls / 100.0 + 1.0));
+			System.out.println("timerMs: " + timerMs);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
 	public static int getTimerMs() {
-		return 1300;
+		return timerMs;
 	}
 }
