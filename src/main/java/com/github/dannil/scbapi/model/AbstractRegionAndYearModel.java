95e6cadd9c0c12400fbe136e94de0e2ba3e60b4e
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-02-12 12:53:11 +0100

    Formatted code and comments

--- src/main/java/com/github/dannil/scbapi/model/AbstractRegionAndYearModel.java
+++ src/main/java/com/github/dannil/scbapi/model/AbstractRegionAndYearModel.java
@@ -1,17 +1,17 @@
 /*
-Copyright 2016 Daniel Nilsson
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License. 
+ * Copyright 2016 Daniel Nilsson
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.github.dannil.scbapi.model;
@@ -68,7 +68,8 @@ public class AbstractRegionAndYearModel<R, Y, V> extends AbstractModel<V> {
 		}
 
 		AbstractRegionAndYearModel<?, ?, ?> other = (AbstractRegionAndYearModel<?, ?, ?>) obj;
-		return super.equals(other) && Objects.equals(this.region, other.region) && Objects.equals(this.year, other.year);
+		return super.equals(other) && Objects.equals(this.region, other.region)
+				&& Objects.equals(this.year, other.year);
 	}
 
 }
