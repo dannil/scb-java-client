62bcdf41f0b8e31135ea486cc5326368e146e19f
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-03-12 17:47:16 +0100

    Removed test code

--- src/main/java/com/github/dannil/scbjavaclient/utility/requester/Requester.java
+++ src/main/java/com/github/dannil/scbjavaclient/utility/requester/Requester.java
@@ -23,14 +23,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-
-import com.github.dannil.scbjavaclient.utility.ListUtility;
-import com.github.dannil.scbjavaclient.utility.QueryBuilder;
 
 /**
  * Class which sends URL requests to the specified address, in this case the SCB API.
@@ -51,18 +46,11 @@ public abstract class Requester {
 		try {
 			input = getClass().getClassLoader().getResourceAsStream("project.properties");
 
-			// load a properties file
 			prop.load(input);
-
-			// gETRequester the property value and print it out
 
 			String artifactId = prop.getProperty("artifactId");
 			String version = prop.getProperty("version");
 			String url = prop.getProperty("url");
-
-			// System.out.println(artifactId);
-			// System.out.println(version);
-			// System.out.println(url);
 
 			this.requestProperties.put("Accept", "application/json");
 			this.requestProperties.put("Content-Type", "application/json; charset=utf-8");
@@ -75,8 +63,6 @@ public abstract class Requester {
 			builder.append(System.getProperty("os.name"));
 
 			this.requestProperties.put("User-Agent", builder.toString());
-			// requestProperties.put();
-			// requestProperties.put();
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
@@ -121,102 +107,6 @@ public abstract class Requester {
 		return builder.toString();
 	}
 
-	// /**
-	// * Perform a GETRequester request to the specified address.
-	// *
-	// * @param address
-	// * the address to perform request to
-	// * @return the response from this request as a string
-	// * @throws IOException
-	// * if the address isn't a valid URL format
-	// */
-	// public String doGet(String address) throws IOException {
-	// StringBuilder builder = new StringBuilder();
-	// URL url = new URL(address);
-	//
-	// HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-	//
-	// connection.setDoInput(true);
-	// connection.setRequestMethod("GETRequester");
-	//
-	// setRequestProperties(connection, "Accept", "Content-Type", "User-Agent");
-	//
-	// try (BufferedReader br = new BufferedReader(new
-	// InputStreamReader(connection.getInputStream(), "utf-8"))) {
-	// // Handle UTF-8 byte order mark (BOM)
-	// br.mark(4);
-	//
-	// // Checks if the stream contains a BOM. If it doesn't, reset the
-	// // stream pointer to the location specified by br.mark()
-	// if ('\uFEFF' != br.read()) {
-	// br.reset();
-	// }
-	//
-	// String line;
-	// while ((line = br.readLine()) != null) {
-	// builder.append(line);
-	// }
-	// }
-	//
-	// connection.disconnect();
-	//
-	// return builder.toString();
-	// }
-	//
-	// /**
-	// * Perform a POST request to the specified address and the specified query (payload).
-	// *
-	// * @param address
-	// * the address to perform request to
-	// * @param query
-	// * the payload to send along
-	// * @return the response from this request as a string
-	// * @throws IOException
-	// * if the address isn't a valid URL format
-	// */
-	// public static String doPost(String address, String query) throws IOException {
-	// StringBuilder builder = new StringBuilder();
-	// URL url = new URL(address);
-	//
-	// HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-	//
-	// connection.setDoInput(true);
-	// connection.setDoOutput(true);
-	// connection.setRequestMethod("POST");
-	// connection.setRequestProperty("Accept", "application/json");
-	// connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
-	// connection.setRequestProperty("User-Agent",
-	// "scb-java-client/VERSION-HERE (github.com/dannil/scb-java-client), " +
-	// System.getProperty("os.name"));
-	//
-	// try (OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(),
-	// "utf-8")) {
-	// writer.write(query);
-	// writer.close();
-	// }
-	//
-	// try (BufferedReader br = new BufferedReader(new
-	// InputStreamReader(connection.getInputStream(), "utf-8"))) {
-	// // Handle UTF-8 byte order mark (BOM)
-	// br.mark(4);
-	//
-	// // Checks if the stream contains a BOM. If it doesn't, reset the
-	// // stream pointer to the location specified by br.mark()
-	// if ('\uFEFF' != br.read()) {
-	// br.reset();
-	// }
-	//
-	// String line;
-	// while ((line = br.readLine()) != null) {
-	// builder.append(line);
-	// }
-	// }
-	//
-	// connection.disconnect();
-	//
-	// return builder.toString();
-	// }
-	//
 	/**
 	 * Return the available codes from the specified table.
 	 *
@@ -235,24 +125,4 @@ public abstract class Requester {
 		return null;
 	}
 
-	public static void main(String[] args) throws IOException {
-		// Requester poster = new Requester();
-		// System.out.println(poster.doGet("http://google.com"));
-		//
-		// System.out.println(poster.post
-		// .doRequest("http://api.scb.se/OV0104/v1/doris/en/ssd/BE/BE0001/BE0001T04Ar", null));
-
-		Requester get = RequesterFactory.getInstance("GET");
-		System.out.println(get.doRequest("http://google.com"));
-
-		Map<String, Collection<?>> mappings = new HashMap<String, Collection<?>>();
-		mappings.put("ContentsCode", ListUtility.toList("BE0701AB"));
-		mappings.put("Region", Arrays.asList("0114"));
-
-		QueryBuilder b = QueryBuilder.getInstance();
-
-		Requester post = RequesterFactory.getInstance("POST");
-		((POSTRequester) post).setPayload(b.build(mappings));
-		System.out.println(post.doRequest("http://api.scb.se/OV0104/v1/doris/en/ssd/BE/BE0701/MedelAlderNY"));
-	}
 }
