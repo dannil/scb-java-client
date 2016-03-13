956e33252ccb0b72224b2db816073781d220ea68
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-03-13 10:58:13 +0100

    Added tests and licenses

--- src/main/java/com/github/dannil/scbjavaclient/utility/requester/AbstractRequester.java
+++ src/main/java/com/github/dannil/scbjavaclient/utility/requester/AbstractRequester.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Daniel Nilsson
+ * Copyright 2016 Daniel Nilsson
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
