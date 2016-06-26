95a189ea0edc4001ba62a8be17054df5de16b205
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-06-26 22:23:28 +0200

    Added unit tests for constant classes

---
+++ src/test/java/com/github/dannil/scbjavaclient/constants/APIConstants_UnitTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2016 Daniel Nilsson
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.dannil.scbjavaclient.constants;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Locale;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class APIConstants_UnitTest {
+
+	@Test
+	public void callPrivateConstructor() throws InstantiationException, IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException {
+		Constructor<?>[] cons = APIConstants.class.getDeclaredConstructors();
+		cons[0].setAccessible(true);
+		cons[0].newInstance();
+		cons[0].setAccessible(false);
+
+		assertFalse(cons[0].isAccessible());
+	}
+
+	@Test
+	public void fallbackLocaleNotNull() {
+		Locale fallback = new Locale("sv", "SE");
+		assertEquals(fallback, APIConstants.FALLBACK_LOCALE);
+	}
+
+}
