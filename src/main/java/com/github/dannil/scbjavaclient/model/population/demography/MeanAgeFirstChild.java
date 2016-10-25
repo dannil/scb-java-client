4ca3226620c4a0112b4308541fda0c8b3fe72763
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-10-25 21:28:11 +0200

    Modified return types and extended interoperability

--- src/main/java/com/github/dannil/scbjavaclient/model/population/demography/MeanAgeFirstChild.java
+++ src/main/java/com/github/dannil/scbjavaclient/model/population/demography/MeanAgeFirstChild.java
@@ -16,6 +16,7 @@
 
 package com.github.dannil.scbjavaclient.model.population.demography;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -126,7 +127,7 @@ public class MeanAgeFirstChild extends AbstractRegionYearAndValueModel<String, I
 	 *
 	 * @return a list of the available codes and their values
 	 */
-	public static Map<String, List<String>> getCodes() {
+	public static Map<String, Collection<String>> getCodes() {
 		AbstractRequester get = RequesterFactory.getRequester(RequestMethod.GET);
 		return JsonUtility.getInputs(get.getBodyAsStringFromTable("BE/BE0701/MedelAlderNY"));
 	}
--- src/main/java/com/github/dannil/scbjavaclient/utility/JsonUtility.java
+++ src/main/java/com/github/dannil/scbjavaclient/utility/JsonUtility.java
@@ -18,6 +18,7 @@ package com.github.dannil.scbjavaclient.utility;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -155,16 +156,7 @@ public final class JsonUtility {
 	 */
 	public static boolean isConventionalJson(JsonNode node) {
 		// Check if the node is correctly formatted
-		if (node.has("columns") || node.has("data") || node.has("comments")) {
-			if (!node.isArray()) {
-				return false;
-			}
-		}
-		// JsonNode e = node.get(0);
-		// if (e != null) {
-		// return e.has("values");
-		// }
-		return true;
+		return !(node.has("columns") || node.has("data") || node.has("comments") || !node.isArray());
 	}
 
 	/**
@@ -176,10 +168,7 @@ public final class JsonUtility {
 	 */
 	public static boolean isQuery(JsonNode node) {
 		// Check if the node is actually a query
-		if (node.has("query")) {
-			return true;
-		}
-		return false;
+		return node.has("query");
 	}
 
 	/**
@@ -229,8 +218,8 @@ public final class JsonUtility {
 	 *            the JSON which should be parsed
 	 * @return a collection of all codes and their respective values
 	 */
-	public static Map<String, List<String>> getInputs(String json) {
-		Map<String, List<String>> inputs = new HashMap<String, List<String>>();
+	public static Map<String, Collection<String>> getInputs(String json) {
+		Map<String, Collection<String>> inputs = new HashMap<String, Collection<String>>();
 
 		JsonNode node = JsonUtility.toNode(json, "variables");
 		if (node == null) {
@@ -260,11 +249,11 @@ public final class JsonUtility {
 	 *             if the JSON doesn't contain the code
 	 */
 	public static List<String> getValues(String json, String code) throws IllegalArgumentException {
-		Map<String, List<String>> inputs = getInputs(json);
+		Map<String, Collection<String>> inputs = getInputs(json);
 		if (!inputs.containsKey(code)) {
 			throw new IllegalArgumentException();
 		}
-		return inputs.get(code);
+		return new ArrayList<String>(inputs.get(code));
 	}
 
 	/**
