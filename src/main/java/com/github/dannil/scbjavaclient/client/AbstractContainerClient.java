1110114ffdd9512c2f42475e9ed2d88a7222f8b5
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-02-20 09:47:23 +0100

    Fixed Checkstyle errors

--- src/main/java/com/github/dannil/scbjavaclient/client/AbstractContainerClient.java
+++ src/main/java/com/github/dannil/scbjavaclient/client/AbstractContainerClient.java
@@ -67,7 +67,7 @@ public abstract class AbstractContainerClient extends AbstractClient {
      * @param client
      *            the client
      */
-    public final void add(AbstractClient client) {
+    public void add(AbstractClient client) {
         this.clients.add(client);
     }
 
