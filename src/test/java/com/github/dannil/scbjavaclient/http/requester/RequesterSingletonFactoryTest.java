973fb3abf7ac1eea6a605895410fb73419a41307
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-03-11 14:44:16 +0100

    Added integration test to check for test annotation

--- src/test/java/com/github/dannil/scbjavaclient/http/requester/RequesterSingletonFactoryTest.java
+++ src/test/java/com/github/dannil/scbjavaclient/http/requester/RequesterSingletonFactoryTest.java
@@ -27,7 +27,10 @@ import java.nio.charset.StandardCharsets;
 import com.github.dannil.scbjavaclient.http.RequestMethod;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class RequesterSingletonFactoryTest {
 
     @Test
