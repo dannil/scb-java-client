739e5a19715aa828e13fd06a3e73591874f168a1
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-03-15 11:04:10 +0100

    Added legal guardians client and model

---
+++ src/main/java/com/github/dannil/scbjavaclient/model/livingconditions/families/legalguardians/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * <p>Package which contains the models for living conditions families legal guardians
+ * data.</p>
+ */
+package com.github.dannil.scbjavaclient.model.livingconditions.families.legalguardians;
