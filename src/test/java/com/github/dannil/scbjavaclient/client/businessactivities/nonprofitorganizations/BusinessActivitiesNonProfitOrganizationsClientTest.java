91fa167ac63a658816c8e9356573e6117517a62c
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-04-13 11:43:03 +0200

    Improved test to check for correct name

--- src/test/java/com/github/dannil/scbjavaclient/client/businessactivities/nonprofitorganizations/BusinessActivitiesNonProfitOrganizationsClientTest.java
+++ src/test/java/com/github/dannil/scbjavaclient/client/businessactivities/nonprofitorganizations/BusinessActivitiesNonProfitOrganizationsClientTest.java
@@ -27,7 +27,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class BusinessActivitiesNonProfitOrganizationsTest {
+public class BusinessActivitiesNonProfitOrganizationsClientTest {
 
     @Test
     public void createWithLocaleConstructor() {
