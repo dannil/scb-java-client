c75cb3f3c06ff6405c657379291a79327768eaba
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-04-16 14:55:16 +0200

    Implemented client for publicly owned enterprises

--- src/main/java/com/github/dannil/scbjavaclient/client/publicfinances/publiclyownedenterprises/PublicFinancesPubliclyOwnedEnterprisesClient.java
+++ src/main/java/com/github/dannil/scbjavaclient/client/publicfinances/publiclyownedenterprises/PublicFinancesPubliclyOwnedEnterprisesClient.java
@@ -39,7 +39,115 @@ public class PublicFinancesPubliclyOwnedEnterprisesClient extends AbstractClient
         super();
     }
 
-    TODO
+    /**
+     * <p>Overloaded constructor.</p>
+     *
+     * @param locale
+     *            the <code>Locale</code> for this client
+     */
+    public PublicFinancesPubliclyOwnedEnterprisesClient(Locale locale) {
+        super(locale);
+    }
+
+    public List<ResponseModel> getDataForMunicipalityOwnedEnterprises() {
+        return getDataForMunicipalityOwnedEnterprises(null, null);
+    }
+
+    public List<ResponseModel> getDataForMunicipalityOwnedEnterprises(Collection<String> regions,
+            Collection<Integer> years) {
+        Map<String, Collection<?>> mappings = new HashMap<>();
+        mappings.put(APIConstants.REGION_CODE, regions);
+        mappings.put(APIConstants.TIME_CODE, years);
+
+        return getResponseModels("KomFtgK", mappings);
+    }
+
+    public List<ResponseModel> getEnterprises() {
+        return getEnterprises(null, null);
+    }
+
+    public List<ResponseModel> getEnterprises(Collection<String> regions, Collection<Integer> years) {
+        Map<String, Collection<?>> mappings = new HashMap<>();
+        mappings.put(APIConstants.REGION_CODE, regions);
+        mappings.put(APIConstants.TIME_CODE, years);
+
+        return getResponseModels("KomFtgK100", mappings);
+    }
+
+    public List<ResponseModel> getCountyCouncilsEnterprises() {
+        return getCountyCouncilsEnterprises(null, null, null);
+    }
+
+    public List<ResponseModel> getCountyCouncilsEnterprises(Collection<String> regions, Collection<String> sectors,
+            Collection<Integer> years) {
+        Map<String, Collection<?>> mappings = new HashMap<>();
+        mappings.put(APIConstants.REGION_CODE, regions);
+        mappings.put(APIConstants.SECTOR_CODE, sectors);
+        mappings.put(APIConstants.TIME_CODE, years);
+
+        return getResponseModels("LtFtgL", mappings);
+    }
+
+    public List<ResponseModel> getPubliclyOwnedEnterprises() {
+        return getPubliclyOwnedEnterprises(null, null, null, null);
+    }
+
+    public List<ResponseModel> getPubliclyOwnedEnterprises(Collection<String> sectors,
+            Collection<Integer> typesOfOwnerships, Collection<Integer> legalEntities, Collection<Integer> years) {
+        Map<String, Collection<?>> mappings = new HashMap<>();
+        mappings.put(APIConstants.SECTOR_CODE, sectors);
+        mappings.put("Agarkategori", typesOfOwnerships);
+        mappings.put("Juridiskform", legalEntities);
+        mappings.put(APIConstants.TIME_CODE, years);
+
+        return getResponseModels("OffFtg", mappings);
+    }
+
+    public List<ResponseModel> getIncomeStatements() {
+        return getIncomeStatements(null, null, null, null);
+    }
+
+    public List<ResponseModel> getIncomeStatements(Collection<String> sectors, Collection<Integer> typesOfOwnerships,
+            Collection<String> industrialClassifications, Collection<Integer> years) {
+        Map<String, Collection<?>> mappings = new HashMap<>();
+        mappings.put(APIConstants.SECTOR_CODE, sectors);
+        mappings.put("Agarkategori", typesOfOwnerships);
+        mappings.put("SNI2007", industrialClassifications);
+        mappings.put(APIConstants.TIME_CODE, years);
+
+        return getResponseModels("OffResultSektor", mappings);
+    }
+
+    public List<ResponseModel> getLiabilitiesAndEquity() {
+        return getLiabilitiesAndEquity(null, null, null, null);
+    }
+
+    public List<ResponseModel> getLiabilitiesAndEquity(Collection<String> sectors,
+            Collection<Integer> typesOfOwnerships, Collection<String> industrialClassifications,
+            Collection<Integer> years) {
+        Map<String, Collection<?>> mappings = new HashMap<>();
+        mappings.put(APIConstants.SECTOR_CODE, sectors);
+        mappings.put("Agarkategori", typesOfOwnerships);
+        mappings.put("SNI2007", industrialClassifications);
+        mappings.put(APIConstants.TIME_CODE, years);
+
+        return getResponseModels("OffSkulderSektor", mappings);
+    }
+
+    public List<ResponseModel> getAssets() {
+        return getAssets(null, null, null, null);
+    }
+
+    public List<ResponseModel> getAssets(Collection<String> sectors, Collection<Integer> typesOfOwnerships,
+            Collection<String> industrialClassifications, Collection<Integer> years) {
+        Map<String, Collection<?>> mappings = new HashMap<>();
+        mappings.put(APIConstants.SECTOR_CODE, sectors);
+        mappings.put("Agarkategori", typesOfOwnerships);
+        mappings.put("SNI2007", industrialClassifications);
+        mappings.put(APIConstants.TIME_CODE, years);
+
+        return getResponseModels("OffTillgSektor", mappings);
+    }
 
     @Override
     public URLEndpoint getUrl() {
