6e0d08dcdcb341580a1f1a33d9fb3b08a0fb819a
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-04-24 19:27:27 +0200

    Added package-info files

---
+++ src/main/java/com/github/dannil/scbjavaclient/client/labourmarket/shorttermemployment/population/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * <p>Package which contains all clients for labour market short-term employment
+ * population data.</p>
+ */
+package com.github.dannil.scbjavaclient.client.labourmarket.shorttermemployment.population;
---
+++ src/main/java/com/github/dannil/scbjavaclient/client/labourmarket/shorttermemployment/sickleave/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * <p>Package which contains all clients for labour market short-term sick leave data.</p>
+ */
+package com.github.dannil.scbjavaclient.client.labourmarket.shorttermemployment.sickleave;
