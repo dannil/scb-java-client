c8902b9958c950f7fba55fbd2060a4095b573ebb
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-04-27 07:51:29 +0200

    Added skeletons for clients

---
+++ src/main/java/com/github/dannil/scbjavaclient/client/population/statistics/deaths/PopulationStatisticsDeathsClient.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2017 Daniel Nilsson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.github.dannil.scbjavaclient.client.population.statistics.deaths;
+
+import java.util.Locale;
+
+import com.github.dannil.scbjavaclient.client.AbstractClient;
+import com.github.dannil.scbjavaclient.http.URLEndpoint;
+
+/**
+ * <p>Client which handles population statistics deaths data fetching.</p>
+ *
+ * @since 0.3.0
+ */
+public class PopulationStatisticsDeathsClient extends AbstractClient {
+
+    /**
+     * <p>Default constructor.</p>
+     */
+    public PopulationStatisticsDeathsClient() {
+        super();
+    }
+
+    /**
+     * <p>Overloaded constructor.</p>
+     *
+     * @param locale
+     *            the <code>Locale</code> for this client
+     */
+    public PopulationStatisticsDeathsClient(Locale locale) {
+        super(locale);
+    }
+    
+    TODO
+
+    @Override
+    public URLEndpoint getUrl() {
+        return getRootUrl().append("BE/BE0101/BE0101I/");
+    }
+
+}
