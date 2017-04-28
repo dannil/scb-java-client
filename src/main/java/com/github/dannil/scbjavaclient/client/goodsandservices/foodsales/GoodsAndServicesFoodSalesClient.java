9de077a2459a9653abf70e82a3fdc434150dc71a
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-04-28 20:13:17 +0200

    Implemented food sales client

--- src/main/java/com/github/dannil/scbjavaclient/client/goodsandservices/foodsales/GoodsAndServicesFoodSalesClient.java
+++ src/main/java/com/github/dannil/scbjavaclient/client/goodsandservices/foodsales/GoodsAndServicesFoodSalesClient.java
@@ -14,10 +14,16 @@
 
 package com.github.dannil.scbjavaclient.client.goodsandservices.foodsales;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import com.github.dannil.scbjavaclient.client.AbstractClient;
+import com.github.dannil.scbjavaclient.constants.APIConstants;
 import com.github.dannil.scbjavaclient.http.URLEndpoint;
+import com.github.dannil.scbjavaclient.model.ResponseModel;
 
 /**
  * <p>Client which handles goods and services food sales data fetching.</p>
@@ -41,6 +47,73 @@ public class GoodsAndServicesFoodSalesClient extends AbstractClient {
      */
     public GoodsAndServicesFoodSalesClient(Locale locale) {
         super(locale);
+    }
+
+    /**
+     * <p>Fetch all turnover of food and drinks data.</p>
+     *
+     * @return the data wrapped in a list of
+     *         {@link com.github.dannil.scbjavaclient.model.ResponseModel ResponseModel}
+     *         objects
+     *
+     * @see #getTurnoverOfFoodAndDrinks(Collection, Collection)
+     */
+    public List<ResponseModel> getTurnoverOfFoodAndDrinks() {
+        return getTurnoverOfFoodAndDrinks(null, null);
+    }
+
+    /**
+     * <p>Fetch all turnover of food and drinks data which match the input
+     * constraints.</p>
+     *
+     * @param productGroups
+     *            the regions to fetch data for
+     * @param years
+     *            the years to fetch data for
+     * @return the data wrapped in a list of
+     *         {@link com.github.dannil.scbjavaclient.model.ResponseModel ResponseModel}
+     *         objects
+     */
+    public List<ResponseModel> getTurnoverOfFoodAndDrinks(Collection<String> productGroups, Collection<Integer> years) {
+        Map<String, Collection<?>> mappings = new HashMap<>();
+        mappings.put("Varugrupp", productGroups);
+        mappings.put(APIConstants.TIME_CODE, years);
+
+        return getResponseModels("Livs", mappings);
+    }
+
+    /**
+     * <p>Fetch all turnover of ecological food and non alcoholic drinks data.</p>
+     *
+     * @return the data wrapped in a list of
+     *         {@link com.github.dannil.scbjavaclient.model.ResponseModel ResponseModel}
+     *         objects
+     *
+     * @see #getTurnoverOfEcologicalFoodAndNonAlcoholicDrinks(Collection, Collection)
+     */
+    public List<ResponseModel> getTurnoverOfEcologicalFoodAndNonAlcoholicDrinks() {
+        return getTurnoverOfEcologicalFoodAndNonAlcoholicDrinks(null, null);
+    }
+
+    /**
+     * <p>Fetch all turnover of ecological food and non alcoholic drinks data which match
+     * the input constraints.</p>
+     *
+     * @param productGroups
+     *            the regions to fetch data for
+     * @param years
+     *            the years to fetch data for
+     * @return the data wrapped in a list of
+     *         {@link com.github.dannil.scbjavaclient.model.ResponseModel ResponseModel}
+     *         objects
+     */
+    public List<ResponseModel> getTurnoverOfEcologicalFoodAndNonAlcoholicDrinks(Collection<String> productGroups,
+            Collection<Integer> years) {
+        Map<String, Collection<?>> mappings = new HashMap<>();
+        mappings.put("Varugrupp", productGroups);
+        mappings.put(APIConstants.TIME_CODE, years);
+
+        return getResponseModels("Livs", mappings);
     }
 
     @Override
