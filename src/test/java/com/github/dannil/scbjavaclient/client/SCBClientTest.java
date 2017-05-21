b8e2ce27d9a36ab1485cf72cfb15344b19073045
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-05-21 11:55:18 +0200

    Added test methods for retrieving sub-clients

--- src/test/java/com/github/dannil/scbjavaclient/client/SCBClientTest.java
+++ src/test/java/com/github/dannil/scbjavaclient/client/SCBClientTest.java
@@ -18,11 +18,20 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Locale;
 
-import com.github.dannil.scbjavaclient.http.URLEndpoint;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import com.github.dannil.scbjavaclient.client.businessactivities.BusinessActivitiesClient;
+import com.github.dannil.scbjavaclient.client.energy.EnergyClient;
+import com.github.dannil.scbjavaclient.client.environment.EnvironmentClient;
+import com.github.dannil.scbjavaclient.client.financialmarkets.FinancialMarketsClient;
+import com.github.dannil.scbjavaclient.client.goodsandservices.GoodsAndServicesClient;
+import com.github.dannil.scbjavaclient.client.labourmarket.LabourMarketClient;
+import com.github.dannil.scbjavaclient.client.population.PopulationClient;
+import com.github.dannil.scbjavaclient.client.publicfinances.PublicFinancesClient;
+import com.github.dannil.scbjavaclient.client.transport.TransportClient;
+import com.github.dannil.scbjavaclient.http.URLEndpoint;
 
 @RunWith(JUnit4.class)
 public class SCBClientTest {
@@ -44,6 +53,69 @@ public class SCBClientTest {
         client.setLocalizationLocale(localizationLocale);
 
         assertEquals(localizationLocale, client.getLocalizationLocale());
+    }
+
+    @Test
+    public void businessActivities() {
+        SCBClient client = new SCBClient();
+
+        assertEquals(client.businessActivities().getClass(), BusinessActivitiesClient.class);
+    }
+
+    @Test
+    public void energy() {
+        SCBClient client = new SCBClient();
+
+        assertEquals(client.energy().getClass(), EnergyClient.class);
+    }
+
+    @Test
+    public void environment() {
+        SCBClient client = new SCBClient();
+
+        assertEquals(client.environment().getClass(), EnvironmentClient.class);
+    }
+
+    @Test
+    public void financialMarkets() {
+        SCBClient client = new SCBClient();
+
+        assertEquals(client.financialMarkets().getClass(), FinancialMarketsClient.class);
+    }
+
+    @Test
+    public void goodsAndServices() {
+        SCBClient client = new SCBClient();
+
+        assertEquals(client.goodsAndServices().getClass(), GoodsAndServicesClient.class);
+    }
+
+    @Test
+    public void labourMarket() {
+        SCBClient client = new SCBClient();
+
+        assertEquals(client.labourMarket().getClass(), LabourMarketClient.class);
+    }
+
+    @Test
+    public void population() {
+        SCBClient client = new SCBClient();
+
+        assertEquals(client.population().getClass(), PopulationClient.class);
+    }
+
+    @Test
+    public void publicFinances() {
+        SCBClient client = new SCBClient();
+
+        assertEquals(client.publicFinances().getClass(), PublicFinancesClient.class);
+    }
+
+    @Test
+    public void transport() {
+        SCBClient client = new SCBClient();
+
+        assertEquals(client.transport().getClass(), TransportClient.class);
     }
 
     @Test
