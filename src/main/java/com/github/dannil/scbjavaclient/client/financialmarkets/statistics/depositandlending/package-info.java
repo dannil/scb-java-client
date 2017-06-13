5faee9d5b30fdab7b89872cd2aac2e8bc894dcc8
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-06-13 20:09:23 +0200

    Removed statistics clients
    These clients are developed in branch
    feat/new-financialmarkets-statistics-client

--- src/main/java/com/github/dannil/scbjavaclient/client/financialmarkets/statistics/depositandlending/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * <p>Package which contains all clients for financial markets statistics deposit and
- * lending data.</p>
- */
-package com.github.dannil.scbjavaclient.client.financialmarkets.statistics.depositandlending;
--- src/main/java/com/github/dannil/scbjavaclient/client/financialmarkets/statistics/economicindicators/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * <p>Package which contains all clients for financial markets statistics economic
- * indicators data.</p>
- */
-package com.github.dannil.scbjavaclient.client.financialmarkets.statistics.economicindicators;
