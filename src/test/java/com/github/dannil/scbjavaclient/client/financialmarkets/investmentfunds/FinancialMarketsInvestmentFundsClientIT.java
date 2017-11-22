67a0bad78e96efeb74c4210fc5903340893268bb
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-11-22 13:07:44 +0100

    Changed to correct runner

--- src/test/java/com/github/dannil/scbjavaclient/client/financialmarkets/investmentfunds/FinancialMarketsInvestmentFundsClientIT.java
+++ src/test/java/com/github/dannil/scbjavaclient/client/financialmarkets/investmentfunds/FinancialMarketsInvestmentFundsClientIT.java
@@ -29,7 +29,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-@RunWith(JUnit4.class)
+@RunWith(DateJUnitRunner.class)
 public class FinancialMarketsInvestmentFundsClientIT extends RemoteIntegrationTestSuite {
 
     private FinancialMarketsInvestmentFundsClient client;
