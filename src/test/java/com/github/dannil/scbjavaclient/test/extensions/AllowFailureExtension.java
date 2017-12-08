15c639ad5a5d2a81306833f510d2bd7054e26a63
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-12-08 23:05:14 +0100

    Logging now uses test class as name for logger
    Still need to figure out how to specify specific pattern to the logger
    progrramatically, as some of the information becomes confusing when
    using the calling test class as the name, such as which line the warning
    originated from

--- src/test/java/com/github/dannil/scbjavaclient/test/extensions/AllowFailureExtension.java
+++ src/test/java/com/github/dannil/scbjavaclient/test/extensions/AllowFailureExtension.java
@@ -17,14 +17,11 @@ public class AllowFailureExtension implements BeforeEachCallback, TestExecutionE
 
     private static final Namespace NAMESPACE = Namespace.create("com", "github", "dannil", "scbjavaclient");
 
-    private static final Logger LOGGER = LogManager.getLogger(AllowFailureExtension.class);
+    // private static final Logger LOGGER =
+    // LogManager.getLogger(AllowFailureExtension.class);
 
     @Override
     public void beforeEach(ExtensionContext context) throws Exception {
-        // System.out.println("in before each");
-
-        System.out.println("------------");
-
         Optional<AnnotatedElement> opElement = context.getElement();
         AllowFailure allowFailure = opElement.get().getDeclaredAnnotation(AllowFailure.class);
         NoticeStrategy strategy = allowFailure.notice();
@@ -35,29 +32,19 @@ public class AllowFailureExtension implements BeforeEachCallback, TestExecutionE
 
     @Override
     public void handleTestExecutionException(ExtensionContext context, Throwable throwable) throws Throwable {
-        // System.out.println("in test execution exception extension");
-
         try {
             // Swallow exception to change test result
         } catch (Throwable t) {
             // Swallow exception to change test result
         }
-
         Store store = context.getStore(NAMESPACE);
         store.put("failed", true);
     }
 
     @Override
     public void afterEach(ExtensionContext context) throws Exception {
-        // System.out.println("in after each extension");
-
-        // Optional<AnnotatedElement> opElement = context.getElement();
-        // AllowFailure a = opElement.get().getDeclaredAnnotation(AllowFailure.class);
-        // NoticeStrategy s = a.notice();
-
         Store store = context.getStore(NAMESPACE);
         Optional<Object> opFailed = Optional.ofNullable(store.get("failed"));
-        // System.out.println("opFailed: " + opFailed.toString());
 
         boolean failed = (boolean) opFailed.orElse(false);
         NoticeStrategy strategy = (NoticeStrategy) store.get("strategy");
@@ -65,72 +52,29 @@ public class AllowFailureExtension implements BeforeEachCallback, TestExecutionE
         String testName = context.getRequiredTestMethod().getName();
 
         if (Objects.equals(NoticeStrategy.NEVER, strategy)) {
-            // System.out.println("------------");
             return;
         } else if (Objects.equals(strategy, NoticeStrategy.ALWAYS)
                 || failed && Objects.equals(strategy, NoticeStrategy.ON_FAILURE)
                 || !failed && Objects.equals(strategy, NoticeStrategy.ON_SUCCESS)) {
             logTest(failed, strategy, className, testName);
         }
-        // System.out.println("------------");
     }
 
-    public void logTest(boolean failed, NoticeStrategy strategy, String className, String testName) {
+    private void logTest(boolean failed, NoticeStrategy strategy, String className, String testName) {
         String failedAsString = (failed ? "FAILED" : "PASSED");
         String strategyAsString = strategy.name();
         String result = "";
 
         if (failed) {
-            result = "changing result of test to PASSED";
+            result = ", changing result to PASSED";
         } else {
-            result = "consider removing the annotation or changing the notice strategy";
+            // result = "consider removing the annotation or changing the notice
+            // strategy";
         }
 
-        LOGGER.warn("Test {}.{} {} and is annotated with @AllowFailure(NoticeStrategy.{}), {}", className, testName,
-                failedAsString, strategyAsString, result);
-        // LOGGER.warn(result);
-
-        // if (passed) {
-        // switch (s) {
-        // case ALWAYS:
-        // LOGGER.warn("Test {}.{} PASSED and is annotated with
-        // @AllowFailure(NoticeStrategy.ALWAYS)",
-        // className, testName);
-        // LOGGER.warn("Consider removing the annotation or changing the notice
-        // strategy");
-        // break;
-        // case TEMPORARY:
-        // LOGGER.warn("Test {}.{} PASSED and is annotated with
-        // @AllowFailure(NoticeStrategy.TEMPORARY)",
-        // className, testName);
-        // LOGGER.warn("Consider removing the annotation or changing the notice
-        // strategy");
-        // break;
-        // case NEVER:
-        // break;
-        // default:
-        // break;
-        // }
-        // } else {
-        // switch (s) {
-        // case ALWAYS:
-        // LOGGER.warn("Test {}.{} FAILED and is annotated with
-        // @AllowFailure(NoticeStrategy.ALWAYS)",
-        // className, testName);
-        // LOGGER.warn("Changing result of test to PASSED");
-        // break;
-        // case TEMPORARY:
-        // LOGGER.warn("Test {}.{} FAILED and is annotated with
-        // @AllowFailure(NoticeStrategy.TEMPORARY)",
-        // className, testName);
-        // LOGGER.warn("Changing result of test to PASSED");
-        // break;
-        // case NEVER:
-        // break;
-        // default:
-        // break;
-        // }
-        // }
+        Logger logger = LogManager.getLogger(className);
+        logger.warn("Test {} {} and is annotated with @AllowFailure(NoticeStrategy.{}){}", testName, failedAsString,
+                strategyAsString, result);
     }
 
 }
