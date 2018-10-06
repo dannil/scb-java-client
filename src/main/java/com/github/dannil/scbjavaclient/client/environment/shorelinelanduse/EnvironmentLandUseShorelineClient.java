c358557a5892708f6eed547f150461f2020d5e8e
Daniel Nilsson <daniel.nilsson94@outlook.com>
2018-10-06 17:42:15 +0200

    Added replacement client

---
+++ src/main/java/com/github/dannil/scbjavaclient/client/environment/shorelinelanduse/EnvironmentLandUseShorelineClient.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2018 Daniel Nilsson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.github.dannil.scbjavaclient.client.environment.shorelinelanduse;
+
+import java.util.Locale;
+
+import com.github.dannil.scbjavaclient.client.AbstractContainerClient;
+import com.github.dannil.scbjavaclient.client.environment.shorelinelanduse.landuse.EnvironmentLandUseShorelineLandUseClient;
+import com.github.dannil.scbjavaclient.communication.URLEndpoint;
+
+/**
+ * <p>Client which handles environment shoreline land use data fetching.</p>
+ *
+ * @since 2.1.0
+ */
+public class EnvironmentLandUseShorelineClient extends AbstractContainerClient {
+
+    /**
+     * <p>Default constructor. Initializes values and creates sub-clients.</p>
+     */
+    public EnvironmentLandUseShorelineClient() {
+        super();
+
+        addClient("landuse", new EnvironmentLandUseShorelineLandUseClient());
+    }
+
+    /**
+     * <p>Overloaded constructor.</p>
+     *
+     * @param locale
+     *            the <code>Locale</code> for this client
+     */
+    public EnvironmentLandUseShorelineClient(Locale locale) {
+        super(locale);
+
+        setLocale(locale);
+    }
+
+    /**
+     * <p>Retrieve the client for interacting with environment shoreline land use land use
+     * data.</p>
+     *
+     * @return a client for environment shoreline land use land use data
+     */
+    public EnvironmentLandUseShorelineLandUseClient landUse() {
+        return (EnvironmentLandUseShorelineLandUseClient) getClient("landuse");
+    }
+
+    @Override
+    public URLEndpoint getUrl() {
+        return getRootUrl().append("MI/MI0812/");
+    }
+
+}
