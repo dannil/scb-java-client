2ae73557744f95b712ff99163ea0c179baa4d82e
Daniel Nilsson <daniel.nilsson94@outlook.com>
2018-11-08 23:13:14 +0100

    Improved sorter logic

--- src/test/java/com/github/dannil/scbjavaclient/test/utility/Sorter.java
+++ src/test/java/com/github/dannil/scbjavaclient/test/utility/Sorter.java
@@ -3,23 +3,60 @@ package com.github.dannil.scbjavaclient.test.utility;
 import java.util.ArrayList;
 import java.util.List;
 
+import info.debatty.java.stringsimilarity.Cosine;
 import info.debatty.java.stringsimilarity.Jaccard;
+import info.debatty.java.stringsimilarity.MetricLCS;
+import info.debatty.java.stringsimilarity.NGram;
+import info.debatty.java.stringsimilarity.experimental.Sift4;
 import info.debatty.java.stringsimilarity.interfaces.StringDistance;
 
 public class Sorter {
 
     public static List<String> sortAccordingTo(List<String> toBeSorted, List<String> accordingTo) {
         String[] sortedArr = new String[toBeSorted.size()];
-        StringDistance dis = new Jaccard();
+
         for (int i = 0; i < toBeSorted.size(); i++) {
             String to = toBeSorted.get(i);
             String toLower = to.toLowerCase();
-            double mostProbableDistance = 1.0;
+            double mostProbableDistance = Double.MAX_VALUE;
             int pos = -1;
             for (int j = 0; j < accordingTo.size(); j++) {
                 String acc = accordingTo.get(j);
-                String accLower = acc.toLowerCase();
-                double distance = dis.distance(toLower, accLower);
+                String accLower = acc.toLowerCase().replaceAll("[^a-zA-Z]", "");
+                System.out.println("ACCLOWER: " + accLower);
+                // StringDistance dis = new Jaccard(Math.min(to.length(), acc.length()));
+
+                StringDistance dis = null;
+                double distance = Double.MAX_VALUE;
+
+                // String toWithoutLast = toLower.substring(0, toLower.length() - 1);
+                // if (accLower.startsWith(toWithoutLast)) {
+                // dis = new MetricLCS();
+                // System.out.println("Using metricLCS");
+                // } else {
+                // dis = new Cosine();
+                // System.out.println("Using cosine");
+                // }
+
+                String toLowerWithoutLast = toLower.substring(0, toLower.length() - 1);
+                if (accLower.startsWith(toLowerWithoutLast)) {
+                    // If the method parameter constitutes the beginning of the API
+                    // parameter, it's extremely likely this is a match
+                    distance = 0.0;
+                    // System.out.println("Using metricLCS");
+                    // dis = new MetricLCS();
+
+                    // int shortest = Math.min(accLower.length(), toLower.length());
+                    //
+                    // distance = dis.distance(toLower.substring(0, shortest),
+                    // accLower.substring(0, shortest));
+                } else {
+                    System.out.println("Using cosine");
+                    dis = new Cosine();
+                    distance = dis.distance(toLower, accLower);
+                }
+
+                // double distance = dis.distance(toLower, accLower);
                 System.out.println("TO: " + to + ", ACC: " + acc + ", DISTANCE: " + distance);
                 if (distance < mostProbableDistance) {
                     mostProbableDistance = distance;
