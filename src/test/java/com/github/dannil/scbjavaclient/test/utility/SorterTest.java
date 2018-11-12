126506da78f57a258ce382cf5caa397d7b1e59a1
Daniel Nilsson <daniel.nilsson94@outlook.com>
2018-11-12 11:03:40 +0100

    Improved sorter logic

--- src/test/java/com/github/dannil/scbjavaclient/test/utility/SorterTest.java
+++ src/test/java/com/github/dannil/scbjavaclient/test/utility/SorterTest.java
@@ -19,6 +19,7 @@ public class SorterTest {
                 "every other year");
         List<String> toBeSorted = Arrays.asList("industrialClassification", "wasteCategories", "years", "properties");
 
+        // Verify that elements are in the correct ordering
         assertNotEquals(accordingTo.indexOf("property"), toBeSorted.indexOf("properties"));
         assertNotEquals(accordingTo.indexOf("industrial classification"),
                 toBeSorted.indexOf("industrialClassification"));
@@ -27,7 +28,7 @@ public class SorterTest {
 
         List<String> sorted = Sorter.sortAccordingTo(toBeSorted, accordingTo);
 
-        // Verify that elements are in the correct position
+        // Verify that elements are in the correct ordering
         assertEquals(accordingTo.indexOf("property"), sorted.indexOf("properties"));
         assertEquals(accordingTo.indexOf("industrial classification"), sorted.indexOf("industrialClassification"));
         assertEquals(accordingTo.indexOf("waste category"), sorted.indexOf("wasteCategories"));
@@ -40,6 +41,7 @@ public class SorterTest {
                 "every other year");
         List<String> toBeSorted = Arrays.asList("properties", "wasteCategories", "industrialClassification", "years");
 
+        // Verify that elements are in the correct ordering
         assertEquals(accordingTo.indexOf("property"), toBeSorted.indexOf("properties"));
         assertNotEquals(accordingTo.indexOf("industrial classification"),
                 toBeSorted.indexOf("industrialClassification"));
@@ -48,7 +50,7 @@ public class SorterTest {
 
         List<String> sorted = Sorter.sortAccordingTo(toBeSorted, accordingTo);
 
-        // Verify that elements are in the correct position
+        // Verify that elements are in the correct ordering
         assertEquals(accordingTo.indexOf("property"), sorted.indexOf("properties"));
         assertEquals(accordingTo.indexOf("industrial classification"), sorted.indexOf("industrialClassification"));
         assertEquals(accordingTo.indexOf("waste category"), sorted.indexOf("wasteCategories"));
@@ -61,6 +63,7 @@ public class SorterTest {
                 "every other year");
         List<String> toBeSorted = Arrays.asList("properties", "industrialClassification", "wasteCategories", "years");
 
+        // Verify that elements are in the correct ordering
         assertEquals(accordingTo.indexOf("property"), toBeSorted.indexOf("properties"));
         assertEquals(accordingTo.indexOf("industrial classification"), toBeSorted.indexOf("industrialClassification"));
         assertEquals(accordingTo.indexOf("waste category"), toBeSorted.indexOf("wasteCategories"));
@@ -68,7 +71,7 @@ public class SorterTest {
 
         List<String> sorted = Sorter.sortAccordingTo(toBeSorted, accordingTo);
 
-        // Verify that elements are in the correct position
+        // Verify that elements are in the correct ordering
         assertEquals(accordingTo.indexOf("property"), sorted.indexOf("properties"));
         assertEquals(accordingTo.indexOf("industrial classification"), sorted.indexOf("industrialClassification"));
         assertEquals(accordingTo.indexOf("waste category"), sorted.indexOf("wasteCategories"));
@@ -76,17 +79,18 @@ public class SorterTest {
     }
 
     @Test
-    public void elementsAreSimilar() {
+    public void elementIsSimilarToAnotherElement() {
         List<String> accordingTo = Arrays.asList("sex", "year of birth", "year");
         List<String> toBeSorted = Arrays.asList("sexes", "yearsOfBirths", "years");
 
+        // Verify that elements are in the correct ordering
         assertEquals(accordingTo.indexOf("sex"), toBeSorted.indexOf("sexes"));
         assertEquals(accordingTo.indexOf("year of birth"), toBeSorted.indexOf("yearsOfBirths"));
         assertEquals(accordingTo.indexOf("year"), toBeSorted.indexOf("years"));
 
         List<String> sorted = Sorter.sortAccordingTo(toBeSorted, accordingTo);
 
-        // Verify that elements are in the correct position
+        // Verify that elements are in the correct ordering
         assertEquals(accordingTo.indexOf("sex"), sorted.indexOf("sexes"));
         assertEquals(accordingTo.indexOf("year of birth"), sorted.indexOf("yearsOfBirths"));
         assertEquals(accordingTo.indexOf("year"), sorted.indexOf("years"));
