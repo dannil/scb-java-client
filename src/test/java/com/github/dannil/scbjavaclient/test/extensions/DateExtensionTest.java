8bd847ca348c5669dcb05b79252807f842d41918
Daniel Nilsson <daniel.nilsson94@outlook.com>
2018-11-17 14:58:31 +0100

    Fixed test logic

--- src/test/java/com/github/dannil/scbjavaclient/test/extensions/DateExtensionTest.java
+++ src/test/java/com/github/dannil/scbjavaclient/test/extensions/DateExtensionTest.java
@@ -7,8 +7,11 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.lang.reflect.AnnotatedElement;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ConditionEvaluationResult;
@@ -16,19 +19,35 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 
 public class DateExtensionTest {
 
+    private final int defaultTestsDayLimit = 7;
+    
+    private int actualTestsDayLimit;
+
     private DateExtension ex;
 
     @BeforeEach
     public void setup() {
         ex = new DateExtension();
+
+        // Assume testsDayLimit is the default for all tests unless we make a call to the
+        // method and specify our own limit
+        modifyTestsDayLimit(defaultTestsDayLimit);
+    }
+
+    @AfterEach
+    public void teardown() {
+        String systemPropertyDayLimit = System.getProperty("testsDayLimit");
+        if (systemPropertyDayLimit == null) {
+            throw new UnsupportedOperationException("testsDayLimit can't be null");
+        }
+        System.setProperty("testsDayLimit", String.valueOf(actualTestsDayLimit));
     }
 
     @Test
     public void evaluateExecutionConditionNoAnnotatedElementPresent() {
-        ExtensionContext context = mock(ExtensionContext.class);
-
         Optional<AnnotatedElement> opElement = Optional.empty();
 
+        ExtensionContext context = mock(ExtensionContext.class);
         when(context.getElement()).thenReturn(opElement);
 
         ConditionEvaluationResult result = ex.evaluateExecutionCondition(context);
@@ -38,10 +57,9 @@ public class DateExtensionTest {
 
     @Test
     public void evaluateExecutionConditionNoDateAnnotationPresent() {
+        Optional<AnnotatedElement> opElement = Optional.empty();
+
         ExtensionContext context = mock(ExtensionContext.class);
-
-        Optional<AnnotatedElement> opElement = Optional.ofNullable(Deprecated.class);
-
         when(context.getElement()).thenReturn(opElement);
 
         ConditionEvaluationResult result = ex.evaluateExecutionCondition(context);
@@ -69,8 +87,10 @@ public class DateExtensionTest {
 
     @Test
     public void evaluateExecutionConditionDateIsInTheFuture() {
+        LocalDate now = LocalDate.now();
+        
         Date date = mock(Date.class);
-        when(date.value()).thenReturn("2099-12-31");
+        when(date.value()).thenReturn(now.plusYears(1).toString());
 
         AnnotatedElement element = mock(AnnotatedElement.class);
         when(element.getAnnotation(Date.class)).thenReturn(date);
@@ -87,6 +107,8 @@ public class DateExtensionTest {
 
     @Test
     public void evaluateExecutionConditionDaysAreLessThanZero() {
+        modifyTestsDayLimit(-1L);
+        
         Date date = mock(Date.class);
         when(date.value()).thenReturn("now");
 
@@ -98,24 +120,17 @@ public class DateExtensionTest {
         ExtensionContext context = mock(ExtensionContext.class);
         when(context.getElement()).thenReturn(opElement);
 
-        // Backup testsDayLimit before modification
-        String dayLimitBackup = System.getProperty("testsDayLimit");
-
-        System.setProperty("testsDayLimit", "-1");
-
         ConditionEvaluationResult result = ex.evaluateExecutionCondition(context);
         assertTrue(result.isDisabled());
         assertEquals("Day limit is less than 0", result.getReason().get());
-        
-        if (dayLimitBackup == null) {
-            System.setProperty("testsDayLimit", "");
-        }
     }
-    
+
     @Test
-    public void evaluateExecutionConditionDateIsInThePresent() {
+    public void evaluateExecutionConditionDateIsInThePast() {
+        LocalDate now = LocalDate.now();
+        
         Date date = mock(Date.class);
-        when(date.value()).thenReturn("1970-01-01");
+        when(date.value()).thenReturn(now.minusYears(1).toString());
 
         AnnotatedElement element = mock(AnnotatedElement.class);
         when(element.getAnnotation(Date.class)).thenReturn(date);
@@ -125,18 +140,45 @@ public class DateExtensionTest {
         ExtensionContext context = mock(ExtensionContext.class);
         when(context.getElement()).thenReturn(opElement);
 
-        // Backup testsDayLimit before modification
-        String dayLimitBackup = System.getProperty("testsDayLimit");
-
-        System.setProperty("testsDayLimit", "7");
-
         ConditionEvaluationResult result = ex.evaluateExecutionCondition(context);
         assertTrue(result.isDisabled());
         assertEquals("Date is not within the day limit", result.getReason().get());
+    }
+
+    @Test
+    public void evaluateExecutionConditionDateIsInsideCutoff() {
+        // This test should always be inside the cutoff, so let's do this calculation
+        // dynamically so the test won't begin to fail in the future
+        LocalDate now = LocalDate.now();
+        LocalDate oneYearAgo = now.minusYears(1);
         
-        if (dayLimitBackup == null) {
-            System.setProperty("testsDayLimit", "");
+        long daysBetween = ChronoUnit.DAYS.between(oneYearAgo, now);
+        LocalDate dateBetween = oneYearAgo.plusDays(daysBetween / 2);
+
+        modifyTestsDayLimit(daysBetween);
+
+        Date date = mock(Date.class);
+        when(date.value()).thenReturn(dateBetween.toString());
+        
+        AnnotatedElement element = mock(AnnotatedElement.class);
+        when(element.getAnnotation(Date.class)).thenReturn(date);
+
+        Optional<AnnotatedElement> opElement = Optional.ofNullable(element);
+
+        ExtensionContext context = mock(ExtensionContext.class);
+        when(context.getElement()).thenReturn(opElement);
+
+        ConditionEvaluationResult result = ex.evaluateExecutionCondition(context);
+        assertFalse(result.isDisabled());
+        assertEquals("Date is within the day limit", result.getReason().get());
+    }
+
+    public void modifyTestsDayLimit(long newValue) {
+        String systemPropertyDayLimit = System.getProperty("testsDayLimit");
+        if (systemPropertyDayLimit == null) {
+            actualTestsDayLimit = defaultTestsDayLimit;
         }
+        System.setProperty("testsDayLimit", String.valueOf(newValue));
     }
 
 }
